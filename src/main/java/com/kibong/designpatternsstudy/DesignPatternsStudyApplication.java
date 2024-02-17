4eccae032edbbc2e530ddf2d0d6ed27fa5398a85
kibongcorders <kkp02052@gmail.com>
2024-02-17 22:28:36 +0900

    Initial commit

---
+++ src/main/java/com/kibong/designpatternsstudy/DesignPatternsStudyApplication.java
@@ -0,0 +1,13 @@
+package com.kibong.designpatternsstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternsStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignPatternsStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kibong/designpatternsstudy/DesignPatternsStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kibong.designpatternsstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternsStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
