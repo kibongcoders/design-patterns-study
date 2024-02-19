d1b35ac6766c7f2e301d9fb110febb33dcd3add1
kibongcorders <kkp02052@gmail.com>
2024-02-19 23:14:16 +0900

    study : 팩토리 메서드 정리하기

--- src/main/java/com/kibong/designpatternsstudy/creational_patterns/factory_method/WhiteShipFactory.java
+++
@@ -1,8 +0,0 @@
-package com.kibong.designpatternsstudy.creational_patterns.factory_method;
-
-public class WhiteShipFactory implements ShipFactory {
-    @Override
-    public Ship createShip() {
-        return new WhiteShip();
-    }
-}
