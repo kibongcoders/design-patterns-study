b08ed4fa717ecc670429df9bb64718443052ad4b
kibongcorders <kkp02052@gmail.com>
2024-03-02 23:22:48 +0900

    study : 브릿지 패턴 init

--- src/main/java/com/kibong/designpatternsstudy/structural_patterns/bridge/before/Champion.java
+++ src/main/java/com/kibong/designpatternsstudy/structural_patterns/bridge/before/Champion.java
@@ -1,6 +1,7 @@
 package com.kibong.designpatternsstudy.structural_patterns.bridge.before;
 
-import me.whiteship.designpatterns._02_structural_patterns._07_bridge._02_after.Skin;
+
+import com.kibong.designpatternsstudy.structural_patterns.bridge.simple.Skin;
 
 public interface Champion extends Skin {
 
---
+++ src/main/java/com/kibong/designpatternsstudy/structural_patterns/bridge/simple/Skin.java
@@ -0,0 +1,5 @@
+package com.kibong.designpatternsstudy.structural_patterns.bridge.simple;
+
+public interface Skin {
+    String getName();
+}
