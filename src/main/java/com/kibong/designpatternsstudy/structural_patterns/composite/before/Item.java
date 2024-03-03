19d2d5bd509c695f01549462057ae948f3a42c4c
kibongcorders <kkp02052@gmail.com>
2024-03-04 08:05:16 +0900

    study : Composite init

---
+++ src/main/java/com/kibong/designpatternsstudy/structural_patterns/composite/before/Item.java
@@ -0,0 +1,17 @@
+package com.kibong.designpatternsstudy.structural_patterns.composite.before;
+
+public class Item {
+
+    private String name;
+
+    private int price;
+
+    public Item(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getPrice() {
+        return this.price;
+    }
+}
