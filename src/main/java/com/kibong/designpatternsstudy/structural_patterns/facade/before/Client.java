298522ee9cf641186cde2258fee4b15cade0ffab
kibongcorders <kkp02052@gmail.com>
2024-03-07 00:36:59 +0900

    study : Facade 패턴 init

--- src/main/java/com/kibong/designpatternsstudy/structural_patterns/facade/before/Client.java
+++ src/main/java/com/kibong/designpatternsstudy/structural_patterns/facade/before/Client.java
@@ -6,12 +6,6 @@ import jakarta.mail.Transport;
 import jakarta.mail.internet.InternetAddress;
 import jakarta.mail.internet.MimeMessage;
 
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
 import java.util.Properties;
 
 public class Client {
