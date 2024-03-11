5d9c18ff34a6381f2b77bc30713da46761ebbd06
kibongcorders <kkp02052@gmail.com>
2024-03-11 23:46:56 +0900

    study : 커맨드 패턴 init

---
+++ src/main/java/com/kibong/designpatternsstudy/behavioral_patterns/command/before/Button.java
@@ -0,0 +1,22 @@
+package com.kibong.designpatternsstudy.behavioral_patterns.command.before;
+
+public class Button {
+
+    private Light light;
+
+    public Button(Light light) {
+        this.light = light;
+    }
+
+    public void press() {
+        light.off();
+    }
+
+    public static void main(String[] args) {
+        Button button = new Button(new Light());
+        button.press();
+        button.press();
+        button.press();
+        button.press();
+    }
+}
