0dc9d977c16639db680420cb4d951f68bf7bef46
kibongcorders <kkp02052@gmail.com>
2024-03-13 23:45:14 +0900

    study : Memento Pattern init

---
+++ src/main/java/com/kibong/designpatternsstudy/behavioral_patterns/memento/before/Game.java
@@ -0,0 +1,26 @@
+package com.kibong.designpatternsstudy.behavioral_patterns.memento.before;
+
+import java.io.Serializable;
+
+public class Game implements Serializable {
+
+    private int redTeamScore;
+
+    private int blueTeamScore;
+
+    public int getRedTeamScore() {
+        return redTeamScore;
+    }
+
+    public void setRedTeamScore(int redTeamScore) {
+        this.redTeamScore = redTeamScore;
+    }
+
+    public int getBlueTeamScore() {
+        return blueTeamScore;
+    }
+
+    public void setBlueTeamScore(int blueTeamScore) {
+        this.blueTeamScore = blueTeamScore;
+    }
+}
